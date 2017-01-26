eb8c95c1bb523ed39b274387e2b469423150f934
Kabenla <kabenla@prometsource.com>
2017-01-26 07:07:13 -0600

    Importing Sql library

--- grails-app/services/com/recomdata/transmart/data/export/DataExportService.groovy
+++ grails-app/services/com/recomdata/transmart/data/export/DataExportService.groovy
@@ -3,6 +3,7 @@ package com.recomdata.transmart.data.export
 import com.recomdata.snp.SnpData
 import com.recomdata.transmart.data.export.exception.DataNotFoundException
 import groovy.json.JsonSlurper
+import groovy.sql.Sql
 import org.apache.commons.lang.StringUtils
 import org.springframework.transaction.annotation.Transactional
 import org.transmartproject.core.ontology.Study
